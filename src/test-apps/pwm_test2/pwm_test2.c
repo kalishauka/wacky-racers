efba5888bd7f47b135de3e9d2b51fc4b9963ef93
Luke Peters <lpe62@uclive.ac.nz>
2023-05-11 11:04:33 +1200

    milestone 2

--- src/test-apps/pwm_test2/pwm_test2.c
+++ src/test-apps/pwm_test2/pwm_test2.c
@@ -41,16 +41,35 @@ static const pwm_cfg_t pwm2_cfg =
     .stop_state = PIO_OUTPUT_LOW
 };
 
+static const pwm_cfg_t pwm3_cfg =
+{
+    .pio = PWM3_PIO,
+    .period = PWM_PERIOD_DIVISOR (PWM_FREQ_HZ),
+    .duty = PWM_DUTY_DIVISOR (PWM_FREQ_HZ, 0),
+    .align = PWM_ALIGN_LEFT,
+    .polarity = PWM_POLARITY_HIGH,
+    .stop_state = PIO_OUTPUT_LOW
+};
 
+static const pwm_cfg_t pwm4_cfg =
+{
+    .pio = PWM4_PIO,
+    .period = PWM_PERIOD_DIVISOR (PWM_FREQ_HZ),
+    .duty = PWM_DUTY_DIVISOR (PWM_FREQ_HZ, 0),
+    .align = PWM_ALIGN_LEFT,
+    .polarity = PWM_POLARITY_HIGH,
+    .stop_state = PIO_OUTPUT_LOW
+};
 
 int
 main (void)
 {
     usb_serial_stdio_init ();
-    pio_config_set (PWM3_PIO, PIO_OUTPUT_LOW);
-    pio_config_set (PWM4_PIO, PIO_OUTPUT_LOW);
+
     pwm_t pwm1;
     pwm_t pwm2;
+    pwm_t pwm3;
+    pwm_t pwm4;
 
     pio_config_set (LED_STATUS_PIO, PIO_OUTPUT_HIGH);
 
@@ -62,33 +81,56 @@ main (void)
     if (! pwm2)
         panic (LED_ERROR_PIO, 2);
            
+    pwm3 = pwm_init (&pwm3_cfg);
+    if (! pwm3)
+        panic (LED_ERROR_PIO, 2);
 
+    pwm4 = pwm_init (&pwm4_cfg);
+    if (! pwm4)
+        panic (LED_ERROR_PIO, 2);
 
-    pwm_channels_start (pwm_channel_mask (pwm1) | pwm_channel_mask (pwm2));
+    pwm_channels_start (pwm_channel_mask (pwm1) | pwm_channel_mask (pwm2) | pwm_channel_mask (pwm3) | pwm_channel_mask (pwm4));
 
     while (1)
     {
-        int duty_cycle;
+        int duty_cycle1;
+        int duty_cycle2;
+        int direction1;
+        int direction2;
+        //If direction = 1, go forward, if = 0, go back
         // Read user input
         char buf[256];
         if (fgets(buf, sizeof(buf), stdin)) {
             // sscanf returns the number of input items successfully matched
-            if (sscanf(buf, "%d",&duty_cycle) == 1) {
-                printf("%d\n", duty_cycle);
+            if (sscanf(buf, "%d %d %d %d",&duty_cycle1, &direction1, &duty_cycle2, &direction2) == 4) {
+                printf("%d, %d, %d, %d\n", duty_cycle1, direction1, duty_cycle2, direction2);
             } else {
                 printf("Invalid input\n");
             }
         }
 
-        // Limit duty cycle to 0-100
-        if (duty_cycle < 0)
-            duty_cycle = 0;
-        else if (duty_cycle > 100)
-            duty_cycle = 100;
 
-        // Update PWM duty cycle
-        pwm_duty_set (pwm1, PWM_DUTY_DIVISOR (PWM_FREQ_HZ, duty_cycle));
-        pwm_duty_set (pwm2, PWM_DUTY_DIVISOR (PWM_FREQ_HZ, duty_cycle));
+        switch (direction1) {
+            case 0:
+                pwm_duty_ppt_set(pwm1, duty_cycle1*10);
+                pwm_duty_ppt_set(pwm3, 0);
+                break;
+            case 1:
+                pwm_duty_ppt_set(pwm3, duty_cycle1*10);
+                pwm_duty_ppt_set(pwm1, 0);
+                break;
+        }
+
+        switch (direction2) {
+            case 0:
+                pwm_duty_ppt_set(pwm2, duty_cycle2*10);
+                pwm_duty_ppt_set(pwm4, 0);
+                break;
+            case 1:
+                pwm_duty_ppt_set(pwm4, duty_cycle2*10);
+                pwm_duty_ppt_set(pwm2, 0);
+                break;
+        }
 
         delay_ms (500);
         pio_output_toggle (LED_STATUS_PIO);
